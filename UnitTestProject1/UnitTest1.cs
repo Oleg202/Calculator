46751e4e109f8126c65d683ea9e65293298fea65
Asus <Asus@ASUS-VIVOBOOK>
2022-10-23 23:25:35 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Calculator;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Form1 form1 = new Form1();
+            float a = 2;
+            string dis = "2";
+            string ext = "4";
+            string result = form1.calculate(a,dis,1);
+            Assert.AreEqual(ext, result, "Eror!");
+
+        }
+    }
+}
